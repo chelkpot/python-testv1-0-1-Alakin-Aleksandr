80aec691a0e26e2a65115e8bd18e826dcd363651
Aleksandr Alakin <alakinsasa2008@gmail.com>
2025-09-18 23:56:40 +0300

    Сделал дз

---
+++ task3.py
@@ -0,0 +1,15 @@
+# tasks/task1.py
+
+
+
+def solve():
+# Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
+    weight=70
+    height=float(1.75)
+    bmi=(weight/height**2)
+    print(bmi)
+   
+
+# Код ниже не трогать! он нужен для тестов
+if __name__ == "__main__":
+    solve()
